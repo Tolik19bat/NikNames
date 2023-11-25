a00c4a197a8ace31623dff3cd0a2303ed05c5e9b
tolik19bat <play.hoffa@gmail.com>
2023-11-25 20:23:43 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -23,13 +23,3 @@ class Validator {
 const checkName = new Validator(expectName);
 checkName.validateUsername(expectName)
 // ==================================================
-// export default class Validator {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//
-//   validateUsername(userName) {
-//     const re = /^(?!(.*\d){4})[^\-_][^\d][a-z\-_0-9]+[^\-_0-9]+$/i;
-//     return re.test(userName);
-//   }
-// }
