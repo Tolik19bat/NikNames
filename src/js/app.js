a9c95bdb6a21d2e49a1a9db97d1c68106eca213f
tolik19bat <play.hoffa@gmail.com>
2023-11-25 20:05:28 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,35 @@
 // TODO: write your code here
-import sum from './basic';
+// import sum from './basic';
+//
+// console.log('worked');
+//
+// console.log(sum([1, 2]));
 
-console.log('worked');
+//=============================================
+//входные данные
+const expectName = "Anna";
 
-console.log(sum([1, 2]));
+class Validator {
+  constructor(name) {
+    this.name = name;
+  }
+  validateUsername(userName) {
+    const re = /^[^A-Za-z0-9]$/
+      console.log(re.test(userName))
+      return re.test(userName);
+    }
+  }
+
+const checkName = new Validator(expectName);
+checkName.validateUsername(expectName)
+// ==================================================
+// export default class Validator {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//
+//   validateUsername(userName) {
+//     const re = /^(?!(.*\d){4})[^\-_][^\d][a-z\-_0-9]+[^\-_0-9]+$/i;
+//     return re.test(userName);
+//   }
+// }
